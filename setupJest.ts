0293e6e3b234437e58342f15b4cb38f4d33406c2
Brandon Domingue <getsaf@gmail.com>
2019-07-31 12:20:57 -0400

    chore: Karma-parallel

---
+++ setupJest.ts
@@ -0,0 +1,1 @@
+console.log('SETTING UP JEST');
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Shallow } from 'shallow-render';
 import { AppComponent } from './app.component';
+import { AppModule } from 'src/app/app.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  let shallow: Shallow<AppComponent>;
+  beforeEach(() => {
+    shallow = new Shallow(AppComponent, AppModule);
   });
 
-  it(`should have as title 'ng8-shallow'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng8-shallow');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng8-shallow!');
+  it('displays an image', async () => {
+    const {find} = await shallow.render();
+    expect(find('img')).toHaveFound(1);
   });
 });
